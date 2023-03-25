7dc6aaf81c8e4093854fb7096267f57202444173
Вадим <124439062+Vadim-Bykin@users.noreply.github.com>
2023-03-25 09:54:52 +0400

    Update laba2.py

--- laba2.py
+++ laba2.py
@@ -4,18 +4,15 @@
 import re
 # словарь с прописными цифрами
 digits = {'0': 'ноль', '1': 'один', '2': 'два', '3': 'три', '4': 'четыре', '5': 'пять', '6': 'шесть', '7': 'семь', '8': 'восемь', '9': 'девять'}
+
 K = 3
-with open('text.txt', 'r') as input_file:
-    while True:
-        a = input_file.readline().split() # читаем строку
-        if not a: # если файл пустой
-            print("Файл *.txt в директории проекта кончился")
-            break
-        # цифры 0 или более раз группа повторяющихся цифр, потом может идти опять группа любых цифр, учитываем \s слева и справа - граница слова
-        pattern = r'\d*(\d)\1{%d,}\d*' % K  # шаблон для поиска последовательностей подходящих под условие задачи
-        for match in re.finditer(pattern, a):  # перебираем все подходящие под шаблон последовательности
-            repeated_dig = match.group(1)  # повторяющаяся цифра лежит в group(1)
-            pattern_repeat = r'([' + repeated_dig + '])\\1{%d,}' % K  # шаблон для поиска только повторяющихся последовательностей в найденной последовательности для подсчета количества повторяющихся цифр   
-            count_repeat = len(re.search(pattern_repeat, match.group(0)).group(0))  # количество повторяющихся цифр в match.group(0) - в исходной поседовательности
-            # здесь после обработки search - в group(0) лежит последовательность только из повторяющихся цифр
-            print(match.group(0) + ' ' + digits[match.group(1)] + ' ' + str(count_repeat))  # результат
+with open('data.txt') as input_file:
+    buffer = input_file.readline().split()
+    pattern = r"^[0-9|\"]\d*(\d)\1{%d,}\d*[$\d|\"|?|!]" % K
+    for item in buffer:
+        for match in re.finditer(pattern, item):
+            num = match.group(0)
+            for ch in ['"', "?", "!"]:
+                if ch in num:
+                    num = num.replace(ch,'')
+            print(num + ' ' + digits[match.group(1)] + ' ' + str(match.group(0).count(match.group(1))))
